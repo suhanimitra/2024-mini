06553bca366261ff4b4a6318c8f5c8b399ae4c39
Suhani Mitra <suhanim@bu.edu>
2024-09-10 17:09:51 -0400

    Update exercise_game.py

--- assignment/exercise_game.py
+++ assignment/exercise_game.py
@@ -7,6 +7,17 @@ import time
 import random
 import json
 
+import firebase_admin
+from firebase_admin import firestore
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+doc_ref = db.collection("game data").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+
 
 N: int = 10
 sample_ms = 10.0
